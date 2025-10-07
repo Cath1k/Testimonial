436309f6c7db8764433780ce19bf04f679da28c9
Cath1k <bhda.psaa@gmail.com>
2025-10-07 02:41:31 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const cards *
+  document.querySelectorAll('.testimonial-card');
+  let currentIndex = 0;
+function showNextCard(){
+  cards.forEach(card => card.style.display = 'none');
+  cards[currentIndex].style.display ='block';
+  currentIndex = ( currentIndex +1) % cards.length; }
+});
+  
